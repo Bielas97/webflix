6309aec3584bee419ed3e9112a190bbe11dbb130
wojciechmusiato <wojciech.musiato@gmail.com>
2018-11-05 16:49:32 +0000

    dodalem pole mail do usera

--- src/main/java/com/app/webflix/repository/PaymentRepository.java
+++ src/main/java/com/app/webflix/repository/PaymentRepository.java
@@ -10,8 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    void addOrUpdatePayment(PaymentDto paymentDto);
-    void deletePayment(Long id);
-    Optional<PaymentDto> getOnePayment(Long id);
-    List<PaymentDto> getAll();
+    
 }
